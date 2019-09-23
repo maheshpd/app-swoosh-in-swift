4e68dedfbb8618ae41f8716d6e64e5b2d0b41178
Mahesh Prasad <mahesh223prasad@gmail.com>
2019-09-24 01:00:24 +0530

    IBAction & data models

---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Mahesh Prasad on 24/09/19.
+//  Copyright © 2019 Mahesh Prasad. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
